cf32d58a92ba4b955a9e0d893b5e06ca6ca8d9a6
Omar <96790364+Omaralaac@users.noreply.github.com>
2026-03-27 15:36:02 +0200

    Update no_fillters.py

--- no_fillters.py
+++ no_fillters.py
@@ -7,8 +7,13 @@ import os
 # ==============================
 # 🔑 بيانات البوت
 # ==============================
-TOKEN = "8601457115:AAFD7Mb5WPVoGhmNWimiQpzAIhJTzLvb5mc"
-CHAT_ID = "1130472857"
+
+import os
+
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+#TOKEN = "8601457115:AAFD7Mb5WPVoGhmNWimiQpzAIhJTzLvb5mc"
+#CHAT_ID = "1130472857"
 
 # ==============================
 # 📁 ملف حفظ المشاريع
